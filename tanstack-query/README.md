744e6bb349466a9463118dadec00255381e26965
surya-moorthy <tssuryamoorthy20092005@gmail.com>
2025-10-16 18:15:50 +0530

    learnt useQuery

--- tanstack-query/README.md
+++ tanstack-query/README.md
@@ -3,3 +3,6 @@
 
 ## QueryClient 
  - The backbone of the tanstack query..
+
+## useQuery Hook
+ - it has all the properties that are needed for fetching data , error handling , loading , pending etc..
--- tanstack-query/src/App.tsx
+++ tanstack-query/src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useQuery } from '@tanstack/react-query'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const {data,isPending,refetch} = useQuery({
+    queryKey : ["todos"],
+    queryFn : getTodos
+  })
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <div>{isPending ? <p>  Is Loading....</p> : JSON.stringify(data?.slice(1,10))}</div>
+       <br />
+       <br />
+       <br />
+       <button onClick={()=>{refetch()}}>refetch</button>
     </>
   )
+}
+
+const getTodos = async () => {
+  await new Promise((resolve)=>  setTimeout(resolve,1000))
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+  return await response.json();
 }
 
 export default App
